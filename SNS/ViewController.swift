a4e01e664f0bedddcaed50a222dee58865f275a7
mktheobro <theobrokuradoru@yahoo.co.jp>
2021-06-10 16:49:07 +0900

    first commit

--- SNS/ViewController.swift
+++ SNS/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func showActivityView(_ sender: UIBarButtonItem) {
+        let controller = UIActivityViewController(activityItems: [imageView.image!],                                              applicationActivities: nil)
+        self.present(controller, animated: true, completion:nil)
+    }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
