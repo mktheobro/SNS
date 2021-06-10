2e6aa61c70194cb4e14ba48bd7fc2cc1f89f3f63
mktheobro <theobrokuradoru@yahoo.co.jp>
2021-06-10 13:15:15 +0900

    Initial Commit

---
+++ SNS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SNS
+//
+//  Created by 久保政人 on 2021/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
